9fd7ba53de63d56895f99fbfdb59e0dc5784a503
qixiaojun <qixiaojun@cmhi.chinamobile.com>
2023-03-27 22:39:25 +0800

    Initial commit

---
+++ app/src/main/java/com/tingzq/tv/App.java
@@ -0,0 +1,39 @@
+package com.tingzq.tv;
+
+import android.app.Application;
+import android.content.Context;
+import android.support.annotation.Nullable;
+
+import androidx.multidex.BuildConfig;
+import androidx.multidex.MultiDex;
+
+import com.orhanobut.logger.AndroidLogAdapter;
+import com.orhanobut.logger.FormatStrategy;
+import com.orhanobut.logger.Logger;
+import com.orhanobut.logger.PrettyFormatStrategy;
+import com.tencent.bugly.crashreport.CrashReport;
+
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        CrashReport.initCrashReport(getApplicationContext(), "4c4e985bd4", true);
+
+        FormatStrategy formatStrategy = PrettyFormatStrategy.newBuilder().tag("TvView").build();
+        Logger.addLogAdapter(new AndroidLogAdapter(formatStrategy) {
+            @Override
+            public boolean isLoggable(int priority, @Nullable @androidx.annotation.Nullable String tag) {
+                return BuildConfig.DEBUG;
+            }
+        });
+
+    }
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        MultiDex.install(getApplicationContext());
+    }
+}
